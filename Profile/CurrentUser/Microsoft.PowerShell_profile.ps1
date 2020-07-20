64c408d7da5df8d540c9356d1bf1f401b99684b4
Martin Binder <xca4037@inhouse.ad.in.gad.de>
2020-07-20 15:39:55 +0200

    Included All Hosts profile

---
+++ Profile/CurrentUser/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+$NuGetApiKey = '#######################'
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+[Net.WebRequest]::DefaultWebProxy = New-Object -TypeName Net.WebProxy( 'http://pro-campus.noc.fiducia.de:8080' )
+[Net.WebRequest]::DefaultWebProxy.Credentials = [Net.CredentialCache]::DefaultNetworkCredentials
+[Net.WebRequest]::DefaultWebProxy.BypassProxyOnLocal = $true
+
+$Personal = [Environment]::GetFolderPath( 'MyDocuments' )
+$MyModules = $Personal + '\WindowsPowershell\Modules'
+$GitRepos = $Personal + '\GIT'
+$ForkedRepos = $GitRepos + '\ForkedRepositories'
+$MyGITModules = $GitRepos+ '\MyRepositories\PowerShell\Modules'
+
+<##>
+If ( $env:PSModulePath -notmatch [regex]::Escape( $MyGITModules ) ) {
+    $Paths = New-Object System.Collections.ArrayList
+    [void]$Paths.AddRange( ( $env:PSModulePath -split ';' | ForEach-Object { $_ -replace ';', '' } ) )
+    [void]$Paths.Add( $MyGITModules )
+    $env:PSModulePath = $Paths -join ';'
+}
+#>
