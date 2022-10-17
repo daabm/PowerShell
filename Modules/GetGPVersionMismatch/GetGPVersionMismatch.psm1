31abac1c043c0886758614dd927c62d276ed7741
Martin Binder <martin_binder@outlook.de>
2022-10-17 14:22:37 +0200

    Multithreaded

--- Modules/GetGPVersionMismatch/GetGPVersionMismatch.psm1
+++ Modules/GetGPVersionMismatch/GetGPVersionMismatch.psm1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 
 Retrieves GPOs in a domain. For each GPO, it determines user and computer versions in AD and sysvol.
-GPOs with version mismatches are returned.
+GPOs with version mismatches are output or returned.
 
 .DESCRIPTION
 
@@ -13,15 +13,8 @@ Domain Controllers.
 GPMC will by default connect to AD on the PDC emulator, but sysvol activities will pick a random sysvol replica
 (site aware if DFS is used for replication). In addition, AD and sysvol replication latency are different.
 
-Imagine the following scenario:
-
-2 Administrators are editing the same policy (all versions zero) at almost the same time, where one admin has a
-sysvol connection to DC 1 and the other has a connection to a different DC 2. Admin 1 edits and commits a write
-to the policy. AD and sysvol versions are updated to 1, both on the PDC. AD replication occurs at some point in
-time. Now Admin 2 edits and commits the next write. AD is already replicated, so AD version is updated from 1 to 2.
-Sysvol replication is outstanding, so sysvol version is updated from 0 to 1. Sysvol replication resolves write
-conflicts with "last writer wins", so after both AD and sysvol replication, the GPO now has AD version 2 and
-sysvol version 1.
+If you store the results in an array variable, you can pipe this variable to the Get-GPVersionMismatch function.
+For this to work, GPOID has an alias "ID", and GPONAME has an alias "DisplayName".
 
 DYNAMIC PARAMETERS
 
@@ -34,25 +27,19 @@ DYNAMIC PARAMETERS
     Accept pipeline input?       False
     Accept wildcard characters?  False
 
-.PARAMETER GPObject
+.PARAMETER GPOID
 
-Use this parameter if you want to pipe a GPO object from Get-GPO to this cmdlet.
-You can also use this to first examine all version mismatches ($errorGPOs = Get-GPVersionMismatch -PassThru)
-and then repair them ($errorGPOs | Get-GPVersionMismatch -Repair)
+Use this parameter to verify a single GPO by GUID. You can also use its alias -ID.
 
 .PARAMETER GPOName
 
 Use this parameter to verify specific GPOs by name. You can specify the full name for a single GPO
-or - with the -regex switch - a regex pattern for multiple GPOs.
+or - with the -regex switch - a regex pattern for multiple GPOs. You can also use its alias -DisplayName.
 
 .PARAMETER Regex
 
 If this switch is present, the GPOName is evaluated as a regex pattern. If ommitted, it is evaluated
 as the exact name of an existing GPO.
-
-.PARAMETER GPOID
-
-Use this parameter to verify a single GPO by GUID.
 
 .PARAMETER ServerNamePattern
 
@@ -77,14 +64,6 @@ errors can NOT be fixed with this cmdlet, you have to repair them on your own.
 
 Return the collection of GP objects that have a version mismatch. By default, this cmdlet does not return anything.
 
-.INPUTS
-
-[[Microsoft.GroupPolicy.GPMGPO]]
-
-.OUTPUTS
-
-[[Microsoft.GroupPolicy.GPMGPO]]
-
 .EXAMPLE
 
 Get-GPVersionMismatch -Domain corp.contoso.com
@@ -97,29 +76,39 @@ Get-GPVersionMismatch -Domain corp.contoso.com -Name "Default Domain Policy" -Re
 
 Gets the "Default Domain Policy" in the corp.contoso.com domain and checks it for a version mismatch. If a mismatch is found, a random registry value is added to administrative templates and removed immediately.
 
+.EXAMPLE
+
+$ErrorGPOs = Get-GPVersionMismatch -PDCOnly
+
+Checks all GPOs in the current user's domain and stores the results in $ErrorGPOs. You can then examine the results and act properly.
+
+.EXAMPLE
+
+$ErrorGPOs | Get-GPVersionMismatch -Repair
+
+Takes the results from the previous example and tries to repair the GPOs.
+
 .NOTES
 
 #>
 
-function Get-GPVersionMismatch
-{
+function Get-GPVersionMismatch {
+
     [CmdletBinding( SupportsShouldProcess = $True, DefaultParameterSetName = 'GpoByName' )]
     [Alias()]
     [OutputType([Microsoft.GroupPolicy.Gpo])]
-    Param
-    (
-        [Parameter( ParameterSetName = 'GPObject', ValueFromPipeline = $True )]
-        [Microsoft.GroupPolicy.Gpo]
-        $GPObject,
-        [Parameter( ParameterSetName = 'GpoByName' )]
+    Param (
+        [Parameter( ParameterSetName = 'GpoByID', ValueFromPipelineByPropertyName = $true )]
+        [Alias( 'ID' )]
+        [System.Guid]
+        $GPOID,
+        [Parameter( ParameterSetName = 'GpoByName', ValueFromPipelineByPropertyName = $true )]
+        [Alias( 'DisplayName' )]
         [String]
         $GPOName,
         [Parameter( ParameterSetName = 'GpoByName' )]
         [Switch]
         $Regex,
-        [Parameter( ParameterSetName = 'GpoByID' )]
-        [System.Guid]
-        $GPOID,
         [Parameter()][String]$ServerNamePattern,
         [Parameter()][Switch]$PdcOnly,
         [Parameter()][Switch]$Repair,
@@ -191,12 +180,7 @@ function Get-GPVersionMismatch
         $DomainDNS = $Domain.DNSRoot
         $PDC       = $Domain.PDCEmulator
 
-    }
-
-    Process {
-        $ErrorGPOs = New-Object -TypeName System.Collections.ArrayList
-
-        Write-Verbose -Message "Enumerating servers in domain $DomainDNS..."
+        Write-Verbose "Enumerating servers in domain $DomainDNS..."
         
         If ( $PdcOnly ) {
             $Servers = @( $PDC )
@@ -207,129 +191,145 @@ function Get-GPVersionMismatch
                 $Servers = $Servers | Where-Object { $_ -match $ServerNamePattern }
             }
         }
-        Write-Verbose -Message "Processing $( $Servers.Count ) servers in domain $DomainDNS."
-
-        $Counter = 0
-        Foreach ( $Server in $Servers ) {
-            $Counter += 1
-            $ActivityParms = @{
-                Activity = 'Processing servers in domain {2} ({0}/{1})' -f $Counter, $Servers.Count, $DomainDNS
-                Status = "Retrieving GPOs from $Server"
-                PercentComplete = ( $Counter - 1 ) * 100 / $Servers.Count
-            }
-            Write-Progress @ActivityParms -Id 0 
-
-            $ErrorGPOsOnCurrentServer = 0
-            $GPParms = @{
-                Server = $Server
+        $TargetServers = [Collections.ArrayList]::new()
+        Foreach ( $Server in $Servers | Sort-Object ) {
+            [void] $TargetServers.Add( [PSCustomObject]@{
+                Name = $Server
                 Domain = $DomainDNS
-            }
+                GetGPOJob = $null
+            } )
+        }
+        Write-Verbose "Processing $( $TargetServers.Count ) servers in domain $DomainDNS."
 
-            $ProcessingTime = ( Measure-Command {
-                If ( $PSCmdlet.ParameterSetName -eq 'GPObject' ){
-                    Write-Verbose -Message "Retrieving GPO from server $Server by object"
-                    [array]$GPOs = @( ( Get-GPO -Name $GPObject.Name @GPParms ) )
-                } ElseIf ( $PSCmdlet.ParameterSetName -eq 'GpoById') {
-                    Write-Verbose -Message "Retrieving GPO from server $Server by GUID"
-                    [array]$GPOs = @( ( Get-GPO -Guid $GPOID @GPParms ) )
-                } ElseIf ( $PSCmdlet.ParameterSetName -eq 'GpoByName' -and $GPOName -and -not $Regex.IsPresent ) {
-                    Write-Verbose -Message "Retrieving GPO from server $Server by name"
-                    [array]$GPOs = @( ( Get-GPO -Name $GPOName @GPParms ) )
-                } Else {
-                    Write-Verbose -Message "Retrieving all GPOs from server $Server"
-                    [array]$GPOs = @( ( Get-GPO -All @GPParms ) ) | Select-Object -Property * 
-                    If ( $PSCmdlet.ParameterSetName -eq 'GpoByName' -and $Regex.IsPresent ){
-                        Write-Verbose -Message "Filtering GPO names by regex $GPOName"
-                        [array]$GPOs = $GPOs | Where-Object { $_.DisplayName -match $GPOName }
+        $ScriptBlockRetrieveGPOs = {
+            $Server = $args[0]
+            $GetGpoCommand = $args[1]
+            $Results = $args[2]
+
+            $GPOs = Invoke-Expression $GetGpoCommand
+
+            Foreach ( $GPO  in $GPOs ) {
+                If ( $GPO.User.DSVersion -ne $GPO.User.SysvolVersion -or $GPO.Computer.DSVersion -ne $GPO.Computer.SysvolVersion ) {
+                    Add-Member -InputObject $GPO -NotePropertyMembers @{
+                        Server = $Server
+                        UserAD = $GPO.User.DSVersion
+                        UserSysvol = $GPO.User.SysvolVersion
+                        ComputerAD = $GPO.Computer.DSVersion
+                        ComputerSysvol = $GPO.Computer.SysvolVersion
                     }
+                    [void]$Results.Add( ( $GPO | Select-Object -Property * ) )
                 }
-            } ).TotalSeconds
-            Write-Verbose "Found $( $GPOs.Count ) GPOs in $( $ProcessingTime.ToString( '0.##' ) ) seconds."
-
-            $ActivityParms = @{
-                Activity = 'Processing servers in domain {2} ({0}/{1})' -f $Counter, $Servers.Count, $DomainDNS
-                Status = "Checking version mismatch GPOs on $Server"
-                PercentComplete = ( $Counter - 1 ) * 100 / $Servers.Count
             }
-            Write-Progress @ActivityParms -Id 0 
+        }
 
-            $ProcessingTime = ( Measure-Command {
-                $Counter2 = 0
-                Foreach ( $GPO  in $GPOs ) {
-                    $Counter2 += 1
-                    $ActivityParms = @{
-                        Activity = 'Processing GPOs on server {2} ({0}/{1})' -f $Counter2, $GPOs.Count, $Server
-                        Status = $GPO.DisplayName
-                        PercentComplete = ( $Counter2 - 1 ) * 100 / $GPOs.Count
-                    }
-                    Write-Progress @ActivityParms -Id 1 -ParentId 0
+        Function New-RunspaceJob {
+            [CmdletBinding()]
+            Param(
+                [Management.Automation.Runspaces.RunspacePool] $Pool,
+                [String] $ScriptBlock,
+                [Array] $Arguments
+            )
+            $Job = [PSCustomObject] @{ Powershell = $null; AsyncResult = $null }
+            $Job.Powershell = [Powershell]::Create().AddScript( $ScriptBlock )
+            Foreach ( $Argument in $Arguments ) {
+                [void] $Job.Powershell.AddArgument( $Argument )
+            }
+            $Job.Powershell.RunspacePool = $Pool
+            $Job.AsyncResult = $Job.Powershell.BeginInvoke()
+            Return $Job
+        }
 
-                    If ( $GPO.User.DSVersion -ne $GPO.User.SysvolVersion -or $GPO.Computer.DSVersion -ne $GPO.Computer.SysvolVersion ) {
-                        $ErrorGPOsOnCurrentServer += 1
-                        Add-Member -InputObject $GPO -MemberType NoteProperty -Name 'Server' -Value $Server
-                        [void]$ErrorGPOs.Add( $GPO )
-                    }
+        # Initialize RunspacePool
+        $RunspacePool = [Management.Automation.Runspaces.RunspaceFactory]::CreateRunspacePool( 16, 256 )
+        $RunspacePool.ThreadOptions = [Management.Automation.Runspaces.PSThreadOptions]::Default
+        $RunspacePool.ApartmentState = [Threading.ApartmentState]::MTA
+        $RunspacePool.Open()
+
+        $ErrorGPOs = [Collections.Arraylist]::Synchronized( [Collections.Arraylist]::new() )
+        # [Collections.ArrayList]::new()
+    }
+
+    Process {
+
+        Foreach ( $Server in $TargetServers ) {
+
+            If ( $PSCmdlet.ParameterSetName -eq 'GPObject' ){
+
+                Write-Verbose "Preparing to retrieve GPOs from server $( $Server.Name ) by object"
+                $GetGpoCommand = "Get-GPO -Name $( $GPObject.Name ) -Server $( $Server.Name ) -Domain $( $Server.Domain )"
+
+            } ElseIf ( $PSCmdlet.ParameterSetName -eq 'GpoById') {
+
+                Write-Verbose "Preparing to retrieve GPOs from server $( $Server.Name ) by GUID"
+                $GetGpoCommand = "Get-GPO -Guid $GPOID -Server $( $Server.Name ) -Domain $( $Server.Domain )"
+
+            } ElseIf ( $PSCmdlet.ParameterSetName -eq 'GpoByName' -and $GPOName -and -not $Regex.IsPresent ) {
+
+                Write-Verbose "Preparing to retrieve GPOs from server $( $Server.Name ) by name"
+                $GetGpoCommand = "Get-GPO -Name $GPOName -Server $( $Server.Name ) -Domain $( $Server.Domain )"
+
+            } Else {
+
+                Write-Verbose "Preparing to retrieve all GPOs from server $( $Server.Name )"
+                $GetGpoCommand = "Get-GPO -All -Server $( $Server.Name ) -Domain $( $Server.Domain )" # | Select-Object -Property *"
+
+                If ( $PSCmdlet.ParameterSetName -eq 'GpoByName' -and $Regex.IsPresent ){
+
+                    Write-Verbose "Filtering GPO names by regex $GPOName"
+                    $GetGpoCommand += ' | Where-Object { $_.DisplayName -match ' + $GPOName + ' }'
                 }
-            } ).TotalSeconds
-            Write-Verbose "Checked $( $GPOs.Count ) GPOs for version mismatch in $( $ProcessingTime.ToString( '0.##' ) ) seconds."
-            If ( $ErrorGPOsOnCurrentServer ) {
-                Write-Warning -Message "Found $ErrorGPOsOnCurrentServer version mismatches on $Server."
             }
+            Write-Verbose "Created GetGpoCommand: $GetGpoCommand"
+            Write-Verbose "Detaching GPO search job for $( $Server.Name )..."
 
-            Write-Progress -Activity ( 'Processing GPOs on server {0}' -f $Server ) -Id 1 -ParentId 0 -PercentComplete 100 -Completed
+            $Server.GetGpoJob = New-RunspaceJob -Pool $RunspacePool -ScriptBlock $ScriptBlockRetrieveGPOs -Arguments @( $Server.Name, $GetGpoCommand, $ErrorGPOs )
 
         }
 
-        Write-Progress -Activity ( 'Processing Servers in domain {0}' -f $DomainDNS ) -Id 0 -PercentComplete 100 -Completed
+        Write-Verbose "Collecting GPO search results..."
+        Foreach ( $Server in $TargetServers ) {
+            Write-Verbose "Collecting results from $( $Server.Name )"
+            $null = $Server.GetGpoJob.Powershell.EndInvoke( $Server.GetGpoJob.AsyncResult )
+        }
 
-        Write-Verbose -Message "Number of Errors found: $( $ErrorGPOs.Count )"
+        Write-Verbose "Number of Errors found: $( $ErrorGPOs.Count )"
 
         If ( $Repair.IsPresent ) {
-            Write-Verbose -Message "Trying to repair version mismatch GPOs on PDCe $PDC."
+            Write-Verbose "Trying to repair version mismatches GPOs on PDCe $PDC."
             $GPParms = @{
                 Server = $PDC
                 Domain = $DomainDNS
             }
 
-            $Counter = 0
             Foreach ( $ErrorGPO in $ErrorGPOs | Where-Object { $_.Server -eq $PDC }) {
-                $Counter += 1
-                $ActivityParms = @{
-                    Activity = 'Trying to repair version mismatch GPOs on PDCe {2} ({0}/{1})' -f $Counter, $ErrorGPOs.Count, $PDC
-                    Status = $ErrorGPO.DisplayName
-                    PercentComplete = ( $Counter - 1 ) * 100 / $ErrorGPOs.Count
-                }
-                Write-Progress @ActivityParms -Id 0 
-
-                If ( $ErrorGPO.Computer.DSVersion -ne $ErrorGPO.Computer.SysvolVersion ) {
-                        Write-Verbose -Message "Repairing $( $ErrorGPO.DisplayName ) computer version mismatch..."
+                If ( $ErrorGPO.ComputerAD -ne $ErrorGPO.ComputerSysvol ) {
+                        Write-Verbose "Repairing $( $ErrorGPO.DisplayName ) computer version mismatch..."
                         $null = Set-GPRegistryValue -Guid $ErrorGPO.ID -Key "HKLM\Software\FixGpoVersionMismatch" -ValueName "FixGPOVersions" -Value 1 -Type DWord @GPParms
                         Start-Sleep -Seconds 1 # allow changes to be committed in AD and Sysvol
                         $null = Remove-GPRegistryValue -Guid $ErrorGPO.ID -Key "HKLM\Software\FixGpoVersionMismatch" -ValueName "FixGPOVersions" @GPParms
                         Start-Sleep -Seconds 1 # allow changes to be committed in AD and Sysvol
                 }
-                If ( $ErrorGPO.User.DSVersion -ne $ErrorGPO.User.SysvolVersion ) {
-                        Write-Verbose -Message "Repairing $( $ErrorGPO.DisplayName ) user version mismatch..."
+                If ( $ErrorGPO.UserAD -ne $ErrorGPO.UserSysvol ) {
+                        Write-Verbose "Repairing $( $ErrorGPO.DisplayName ) user version mismatch..."
                         $null = Set-GPRegistryValue -Guid $ErrorGPO.ID -Key "HKCU\Software\FixGpoVersionMismatch" -ValueName "FixGPOVersions" -Value 1 -Type DWord @GPParms
                         Start-Sleep -Seconds 1 # allow changes to be committed in AD and Sysvol
                         $null = Remove-GPRegistryValue -Guid $ErrorGPO.ID -Key "HKCU\Software\FixGpoVersionMismatch" -ValueName "FixGPOVersions" @GPParms
                         Start-Sleep -Seconds 1 # allow changes to be committed in AD and Sysvol
                 }
-
-
             }
-
-            Write-Progress -Activity ( 'Processing version mismatch GPOs in domain {0}' -f $DomainDNS ) -Id 0 -PercentComplete 100 -Completed
         }
     }
 
     End {
-
-        If ( $PassThru.IsPresent ) { $ErrorGPOs }
-
+        $RunspacePool.Close()
+        $RunspacePool.Dispose()
+        If ( $PassThru ) { 
+            $ErrorGPOs
+        } Else {
+            $ErrorGPOs | Format-Table -Property ID, DisplayName, Server, UserAD, UserSysvol, ComputerAD, ComputerSysvol
+        }
     }
 }
-
 
 Function New-DynamicParameter {
     # based on the work of adamtheautomator
@@ -405,6 +405,6 @@ Function New-DynamicParameter {
     }
 
     Return $RuntimeParam
-	
+
 }
 
